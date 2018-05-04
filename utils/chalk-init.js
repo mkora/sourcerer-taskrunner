d652ce4ea92abd3b795d5eb4b0e78281ab786619
mkor <31717889+mkora@users.noreply.github.com>
2018-05-04 09:28:58 -0700

    Add emojis to log messages

--- utils/chalk-init.js
+++ utils/chalk-init.js
@@ -1,10 +1,10 @@
 const logIt = console.log;
 const chalk = require('chalk');
 
-const error = msg => logIt(chalk.bold.red(msg));
-const warn = msg => logIt(chalk.keyword('orange')(msg));
-const success = msg => logIt(chalk.bold.green(msg));
-const debug = msg => logIt(chalk.gray(msg));
+const error = msg => logIt(`⛔️ ${chalk.bold.red(msg)}`);
+const warn = msg => logIt(`⚠️ ${chalk.keyword('orange')(msg)}`);
+const success = msg => logIt(`✅ ${chalk.bold.green(msg)}`);
+const debug = msg => logIt(`☑️ ${chalk.gray(msg)}`);
 const log = msg => logIt(chalk.white(msg));
 
 module.exports = {
