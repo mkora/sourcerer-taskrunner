ec42611101218482e0113c5e0153abf4fb5f3558
mkor <31717889+mkora@users.noreply.github.com>
2018-05-06 19:10:57 -0400

    Add log symbols

--- utils/chalk-init.js
+++ utils/chalk-init.js
@@ -1,10 +1,11 @@
 const log = require('console-log-level')({ level: 'info' });
 const chalk = require('chalk');
+const logSymbols = require('log-symbols');
 
-const error = msg => log.error(`⛔️ ${chalk.bold.red(msg)}`);
-const warn = msg => log.warn(`⚠️ ${chalk.keyword('orange')(msg)}`);
-const success = msg => log.info(`✅ ${chalk.bold.green(msg)}`);
-const debug = msg => log.debug(`☑️ ${chalk.gray(msg)}`);
+const error = msg => log.error(`${logSymbols.error} ${chalk.bold.red(msg)}`);
+const warn = msg => log.warn(`${logSymbols.warning} ${chalk.keyword('orange')(msg)}`);
+const success = msg => log.info(`${logSymbols.success} ${chalk.bold.green(msg)}`);
+const debug = msg => log.debug(`${logSymbols.info} ${chalk.gray(msg)}`);
 const message = msg => console.log(chalk.white(msg));
 
 module.exports = {
