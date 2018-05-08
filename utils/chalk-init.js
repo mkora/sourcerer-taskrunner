66896b3e8a01f32b41332d5e9573f0bebdc87ba4
mkor <31717889+mkora@users.noreply.github.com>
2018-05-08 19:08:24 -0400

    Update log function

--- utils/chalk-init.js
+++ utils/chalk-init.js
@@ -1,17 +1,17 @@
-const log = require('console-log-level')({ level: 'info' });
+const logIt = require('console-log-level')({ level: 'info' });
 const chalk = require('chalk');
 const logSymbols = require('log-symbols');
 
-const error = msg => log.error(`${logSymbols.error} ${chalk.bold.red(msg)}`);
-const warn = msg => log.warn(`${logSymbols.warning} ${chalk.keyword('orange')(msg)}`);
-const success = msg => log.info(`${logSymbols.success} ${chalk.bold.green(msg)}`);
-const debug = msg => log.debug(`${logSymbols.info} ${chalk.gray(msg)}`);
-const message = msg => console.log(chalk.white(msg));
+const error = msg => logIt.error(`${logSymbols.error} ${chalk.bold.red(msg)}`);
+const warn = msg => logIt.warn(`${logSymbols.warning} ${chalk.keyword('orange')(msg)}`);
+const success = msg => logIt.info(`${logSymbols.success} ${chalk.bold.green(msg)}`);
+const debug = msg => logIt.debug(`${logSymbols.info} ${chalk.gray(msg)}`);
+const log = msg => console.log(chalk.white(msg));
 
 module.exports = {
   error,
   warn,
   success,
   debug,
-  message,
+  log,
 };
